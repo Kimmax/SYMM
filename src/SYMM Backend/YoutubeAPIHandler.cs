58c0faa13e33803904419f43824c9807d2e5f077
Kim-Maximilian Nürnberger <git@kim-nuernberger.eu>
2015-05-17 18:59:05 +0200

    Added implementing PlayList and VideoItems

---
+++ src/SYMM Backend/YoutubeAPIHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace SYMM_Backend
+{
+    class YoutubeAPIHandler
+    {
+        private readonly string _APIKey;
+        public string APIKey
+        {
+            get { return _APIKey; }
+        }
+
+        private WebClient wClient = new WebClient();
+
+        public YoutubeAPIHandler(string APIKey)
+        {
+            this._APIKey = APIKey;
+        }
+        
+    }
+}
