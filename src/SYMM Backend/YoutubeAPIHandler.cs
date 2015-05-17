a9a3c8987fa138eef029682ad26d52dc2df26bad
Kim-Maximilian Nürnberger <git@kim-nuernberger.eu>
2015-05-17 19:02:14 +0200

    Removed own API implementation

--- src/SYMM Backend/YoutubeAPIHandler.cs
+++ src/SYMM Backend/YoutubeAPIHandler.cs
@@ -16,39 +16,10 @@ namespace SYMM_Backend
             get { return _APIKey; }
         }
 
-        private const string YoutubeApiBaseURL = "https://www.googleapis.com/youtube/v3/";
-        private const string PlaylistAPIRequestURL = YoutubeApiBaseURL + "playlistItems?part=snippet&playlistId={1}&maxResults={0}&key={2}";
-        private const string VideoItemAPIRequestURL = YoutubeApiBaseURL + "videos?part=snippet&id={0}&maxResults={1}&key={2}";
-
-        private WebClient wClient = new WebClient();
-
         public YoutubeAPIHandler(string APIKey)
         {
             this._APIKey = APIKey;
         }
 
-        private string FormPlayListRequest(string playlistID, int maxresult = 50, string nextPageToken = "")
-        {
-            if (String.IsNullOrEmpty(nextPageToken))
-            {
-                return String.Format(PlaylistAPIRequestURL, playlistID, maxresult, this.APIKey);
-            }
-            else
-            {
-                return String.Format(PlaylistAPIRequestURL, playlistID, maxresult, this.APIKey) + "&pageToken=" + nextPageToken;
-            }
-        }
-
-        private string FormVideoDetailRequest(string videoIDs, int maxresult = 50, string nextPageToken = "")
-        {
-            if (String.IsNullOrEmpty(nextPageToken))
-            {
-                return String.Format(PlaylistAPIRequestURL, Regex.Replace(videoIDs, ",", "%2"), maxresult, this.APIKey);
-            }
-            else
-            {
-                return String.Format(PlaylistAPIRequestURL, Regex.Replace(videoIDs, ",", "%2"), maxresult, this.APIKey) + "&pageToken=" + nextPageToken;
-            }
-        }
     }
 }
