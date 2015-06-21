65a95244c5e2a1880107c37190764e5a88e774db
Kim-Maximilian Nürnberger <git@kim-nuernberger.eu>
2015-06-21 22:41:53 +0200

    Modern UI working, collect page done

---
+++ src/SYMM Frontend WPF/Pages/Erfassen.xaml.cs
@@ -0,0 +1,35 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SYMM_Frontend_WPF.Dialogs;
+
+namespace SYMM_Frontend_WPF.Pages
+{
+    /// <summary>
+    /// Interaktionslogik für Erfassen.xaml
+    /// </summary>
+    public partial class Erfassen : UserControl
+    {
+        public Erfassen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCollectByChanname_Click(object sender, RoutedEventArgs e)
+        {
+            CollectByNameDialog dialog = new CollectByNameDialog();
+            dialog.ShowDialog();
+        }
+    }
+}
