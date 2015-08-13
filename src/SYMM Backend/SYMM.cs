6a898e7a88fbdd933c077a49be581c562dfa2d63
Kim-Maximilian Nürnberger <git@kim-nuernberger.eu>
2015-08-14 00:12:06 +0200

    Implemented Video downloading in the API Handler

--- src/SYMM Backend/SYMM.cs
+++ src/SYMM Backend/SYMM.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace SYMM_Backend
 {
@@ -11,6 +12,10 @@ namespace SYMM_Backend
 
         public event EventHandler<VideoInformationLoadedEventArgs> OnVideoInformationLoaded;
         public event EventHandler<AllVideoInformationLoadedEventArgs> OnAllVideoInformationLoaded;
+
+        public event EventHandler<ProgressEventArgs> OnVideoDownloadProgressChanged;
+        public event EventHandler<ProgressEventArgs> OnVideoAudioExtractionProgressChanged;
+        public event EventHandler<VideoDownloadCompleteEventArgs> OnVideoDownloadComplete;
 
         private readonly string _APIKey;
         public string APIKey
@@ -44,6 +49,30 @@ namespace SYMM_Backend
             };
 
             YouTubeHandler.LoadChannelVideos(channelName);
+        }
+
+        public void DownloadVideoNonBlocking(YouTubeVideo video, string dest)
+        {
+            VideoDownloader downloader = new VideoDownloader(video);
+            downloader.DownloadProgressChanged += (s, e) =>
+            {
+                if (OnVideoDownloadProgressChanged != null)
+                    OnVideoDownloadProgressChanged(this, e);
+            };
+
+            downloader.AudioExtractionProgressChanged += (s, e) =>
+            {
+                if (OnVideoAudioExtractionProgressChanged != null)
+                    OnVideoAudioExtractionProgressChanged(this, e);
+            };
+
+            downloader.VideoDownloadComplete += (s, e) =>
+            {
+                if (OnVideoDownloadComplete != null)
+                    OnVideoDownloadComplete(this, e);
+            };
+
+            new Thread(() => { downloader.DownloadVideo(dest); }).Start();
         }
     }
 }
