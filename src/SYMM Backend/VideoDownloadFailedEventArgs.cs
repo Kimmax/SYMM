05984ed121497e5701ec32c0c7b2a4af47b4b7ec
Kim-Maximilian Nürnberger <git@kim-nuernberger.eu>
2015-08-15 13:41:17 +0200

    Event args for failed donwload

---
+++ src/SYMM Backend/VideoDownloadFailedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SYMM_Backend
+{
+    public class VideoDownloadFailedEventArgs : EventArgs
+    {
+        public VideoDownloadFailedEventArgs(YouTubeVideo video, Exception innerException)
+        {
+            this.Video = video;
+            this.InnerException = innerException;
+        }
+
+        /// <summary>
+        /// Returns the completed video object
+        /// </summary>
+        public YouTubeVideo Video { get; private set; }
+        public Exception InnerException { get; private set; }
+    }
+}
